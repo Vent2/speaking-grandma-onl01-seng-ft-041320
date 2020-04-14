04875153679739005897c9b1f16d940e87085ef1
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-14 14:17:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,6 @@ def speak_to_grandma(speak)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif speak == speak.upcase
   return "NO, NOT SINCE 1938!"
-else speak == ""
+else
   return "HUH?! SPEAK UP, SONNY!"
 end
