6f8ddd1079bd70979b56fdc855adcfa9870e0c7d
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-14 14:24:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
-speak = "I LOVE YOU GRANDMA!"
+phrase = "Hi!"
 
-def speak_to_grandma(speak)
-  if speak == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif speak == speak.upcase
   return "NO, NOT SINCE 1938!"
