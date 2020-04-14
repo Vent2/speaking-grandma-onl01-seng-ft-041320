7122323987df365b883d8a38cb5466de4cec9d14
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-14 14:27:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,14 @@
-phrase = "Hi Nana, how are you?"
+input_phrase = "Hi Nana, how are you?"
+def speak_to_grandma(phrase)  
+  # Check if string equals same phrase all upper case letters, which means string is all uppercase
+  if phrase == phrase.upcase 
+    # return this string if condition is true 
+    puts "NO, NOT SINCE 1938!"
+  else 
+    # return this string if condition is false 
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
 
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif speak == speak.upcase
-  return "NO, NOT SINCE 1938!"
-else
-  return "HUH?! SPEAK UP, SONNY!"
-end
-end
+# execute function passing input_phrase variable as argument
+speak_to_grandma(input_phrase)
