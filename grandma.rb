1e871bc7ce67bef2db4488762ade87a69feda8f3
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-14 14:35:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ input_phrase = "I LOVE YOU GRANDMA!"
 
 def speak_to_grandma(phrase)  
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LVOE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase 
     return "NO, NOT SINCE 1938!"
   else 
