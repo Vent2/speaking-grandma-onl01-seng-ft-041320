64d7d52dea90a5e1c9f897a790b3f8a15e5682cd
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-14 14:34:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 input_phrase = "Hi Nana, how are you?"
 
 def speak_to_grandma(phrase)  
-  if phrase == phrase.upcase 
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LVOE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase 
     return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
